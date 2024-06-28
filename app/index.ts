56af09e0f3c33100ce22071f53830b9e9bfcdb43
Vadim <vadym.perkin@altran.com>
2024-06-28 13:26:08 +0400

    Added more tests and 1 PO

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 import { PageHolder } from "./abstract";
 import { WaitCondition } from "../app/page/waitCondition.page";
+import { Form } from "../app/page/form.page"
 
 export class Application extends PageHolder {
     public waitCondition = new WaitCondition(this.page);
+    public form = new Form(this.page);
 }
