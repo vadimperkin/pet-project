c91c6e8e4d676d80f20b2ec5cf3d8212eb9a54ff
Vadim Perkin <vperkinv@gmail.com>
2024-07-19 16:03:54 +0400

    Added negative test case for ordering pizaa

--- tests/fixture/orderPizza.test.ts
+++ tests/fixture/orderPizza.test.ts
@@ -1,16 +1,32 @@
 import { loginFixture } from "../../fixtures";
-import type { Ingredients, } from "../../app/models";
+import type { Ingredients } from "../../app/models";
+import { createPositiveIngredients } from "../../helpers/constraints";
 
-const ingredients: Ingredients = {
+const ingredientsPositive: Ingredients = createPositiveIngredients({
     size: "small",
     flavour: "Supreme",
     sauce: "Buffalo",
     topping: "Green Olive",
     quantity: 2
+})
+
+const ingredientsNegative: Ingredients = {
+    size: "medium",
+    flavour: "Pepperoni",
+    sauce: "Barbeque",
+    topping: "Onions",
+    quantity: -1
 }
 
-loginFixture("check login flow", async ({ app }) => {
-    await app.orderSubmit.compoundPizza(ingredients);
+loginFixture("check positive ordering pizza flow", async ({ app }) => {
+    await app.orderSubmit.compoundPizza(ingredientsPositive);
     await app.orderSubmit.addPizzaToCart();
+    await app.orderSubmit.verifyAddingToCart();
     await app.orderSubmit.verifyCompoundedPizza();
+});
+
+loginFixture("check negative ordering pizza flow", async ({ app }) => {
+    await app.orderSubmit.compoundPizza(ingredientsNegative);
+    await app.orderSubmit.addPizzaToCart();
+    await app.orderSubmit.verifyNegativeQtyError();
 });
