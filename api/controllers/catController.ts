5caf30a0fed4668f2d3bb8198dff3a4a9be4d895
Vadim Perkin <vperkinv@gmail.com>
2024-07-29 09:51:28 +0400

    Added API

---
+++ api/controllers/catController.ts
@@ -0,0 +1,14 @@
+import { Base } from "../abstract";
+import type { CatBreeds } from "../models";
+
+export class CatController extends Base {
+    /**
+     * Get cats by breed.
+     * @param breed {String} breed you want to find
+     * @returns array of breeds objects
+     */
+    async getCats(breed: string, limit: number): Promise<CatBreeds> {
+        const response = await this.request.get(`https://api.thecatapi.com/v1/images/search?limit=${limit}&breed_ids=${breed}&api_key=${process.env.API_KEY}`);
+        return response.json() as Promise<CatBreeds>;
+    }
+}
