fc46a79e9592687bfafa7271790aff02e645d619
Vadym Perkin <vperkinv@gmail.com>
2024-10-28 19:23:31 +0400

    Created sender 11

--- helpers/reportSender.js
+++ helpers/reportSender.js
@@ -11,6 +11,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const mailOptions = {
+  from: "someAnonymousUserTest@gmail.com",
   to: process.env.EMAIL_RECEIVER,
   subject: 'GitHub "Pet-projects" test report ',
   text: 'Here you can find actual test report -  https://vadimperkin.github.io/pet-project', 
