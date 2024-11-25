a5bc5d23893544f37236729a578840a5c8a0c9b5
Vadim Perkin <vperkinv@gmail.com>
2024-11-25 21:12:32 +0400

    Added extended fixture, added example of keyword-driven test (#21)

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -2,18 +2,20 @@ import { expect, test } from "@playwright/test";
 import { Application } from "../app";
 import { API } from "../api";
 
-export const baseFixture = test.extend<{ App: Application, Api: API }>({
+export const baseFixture = test.extend<{ App: Application; Api: API }>({
   App: async ({ page }, use) => {
     const app = new Application(page);
     await use(app);
   },
-  Api: async({ request }, use) => {
+  Api: async ({ request }, use) => {
     const api = new API(request);
     await use(api);
-  }
+  },
 });
 
-export const loginFixture = baseFixture.extend<DefaultAdmin & { App: Application }>({
+export const loginFixture = baseFixture.extend<
+  DefaultAdmin & { App: Application }
+>({
   defaultAdmin: [
     {
       username: "randomName@test.com",
@@ -29,6 +31,46 @@ export const loginFixture = baseFixture.extend<DefaultAdmin & { App: Application
     await expect(App.auth.addToCart).toBeVisible();
     await use(App);
 
-    console.log("Teard down: executes after test"); 
-  }
+    console.log("Teard down: executes after test");
+  },
+});
+
+export const loginAndOrderPizza = loginFixture.extend<
+  DefaultIngredients & { App: Application }
+>({
+  testOptions: [
+    {
+      ingredients: [
+        {
+          size: "large",
+          flavour: "Cheese",
+          sauce: "Buffalo",
+          topping: "Onions",
+          quantity: 1,
+        },
+      ],
+    },
+    {
+      option: true,
+    },
+  ],
+
+  App: async ({ App, testOptions }, use) => {
+    for (const pizza of testOptions.ingredients) {
+      try {
+        await App.orderSubmit.compoundPizza(pizza);
+        await App.orderSubmit.addPizzaToCart();
+        await App.orderSubmit.verifyAddingToCart();
+        await App.orderSubmit.verifyCompoundedPizza();
+        console.log(
+          `New pizza with: ${JSON.stringify(
+            pizza
+          )} has been successfully added!`
+        );
+      } catch (error) {
+        throw new Error(`Error with adding ${pizza} !`);
+      }
+    }
+    await use(App);
+  },
 });
